6d512f572b9f5c6aeaa8f063c50de4612faa12d0
mca265 <mahimchaudhry4@gmail.com>
2025-03-08 15:13:31 -0800

    finish ui

--- madchat/src/App.js
+++ madchat/src/App.js
@@ -1,6 +1,28 @@
 const App = () => {
   return (
     <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Angry Bot pvt. ltd.</p> 
+        </nav>
+      </section>
+      <section className="main">
+        <h1>Angry Bot</h1>
+        <ul className="feed">
+          
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+          A chatbot with an attitude—Angry Bot starts annoyed and only gets worse.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
