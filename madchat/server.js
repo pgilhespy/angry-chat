f41361af5a7d503f51fa0586ffcbf75ccf0d9a68
mca265 <mahimchaudhry4@gmail.com>
2025-03-08 17:51:18 -0800

    finish backend

--- madchat/server.js
+++ madchat/server.js
@@ -1,8 +1,33 @@
 const PORT = 8000
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(PORT,  () => console.log('Your server is runnin on PORT ' + PORT))
+const API_KEY= process.env.API_KEY
+
+app.post('/completions', async(req, res) => {
+    const options = {
+        method:"POST",
+        header: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "placeholder",
+            messages: [{ role: "user", content: req.body.message}],
+            max_tokens: 1000
+        })
+    }
+    try{
+        const response = await fetch('placeholder', options)
+        const data = await response.json()
+        res.send(data)
+    } catch(error) {
+        console.error(error)
+    }
+})
+
+app.listen(PORT,  () => console.log('Your server is running on PORT ' + PORT))
